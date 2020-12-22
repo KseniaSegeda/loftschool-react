147f0ddcd88df7a73b5709c2121701e7bd4fdafa
KseniaSegeda <ksenia6397@gmail.com>
2020-12-22 07:52:10 +0300

    feat: added map

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import "./Map.css";
 
-const Map = () => (
-    <h2>Map</h2>
-)
+const Map = () => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZHJhZ2luIiwiYSI6ImNraXpob3drMDFvNmcyem56NDI1c2prMTIifQ.zfgJa232mWsv5DD2AQmd_w';
+    const mapContainer = useRef(null);
+    useEffect(() => {
+        const myMap = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: "mapbox://styles/mapbox/streets-v11",
+            center: [37.61513, 55.7513461], // moscow
+            zoom: 11,
+        });
+        return () => myMap.remove();
+    });
+    return <div className="map-container" ref={mapContainer} />;
+}
+
 
 export default Map;
