b7fc4fdb69b4f96e5b6f2931e1fb617aecea79be
KseniaSegeda <ksenia6397@gmail.com>
2020-12-31 06:45:24 +0300

    test: map test  fix

--- src/page/Map/Map.test.js
+++ src/page/Map/Map.test.js
@@ -2,8 +2,17 @@ import ReactDOM from "react-dom";
 import Map from "../../page/Map/Map";
 import React from "react";
 import renderer from "react-test-renderer";
+import { render } from '@testing-library/react'
+import mapboxgl from "mapbox-gl";
+
 jest.mock('mapbox-gl', () => ({
-    Map: jest.fn()
+    GeolocateControl: jest.fn(),
+    Map: jest.fn(() => ({
+        addControl: jest.fn(),
+        on: jest.fn(),
+        remove: jest.fn()
+    })),
+    NavigationControl: jest.fn()
 }));
 
 describe('Map.js', () => {
@@ -11,6 +20,16 @@ describe('Map.js', () => {
         const div = document.createElement("div");
         ReactDOM.render(<Map/>, div);
         ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('should call mapbox-gl', function () {
+        const { getByTestId } = render( <Map />);
+        expect(mapboxgl.Map).toHaveBeenCalledWith({
+            container: getByTestId('map'),
+            style: "mapbox://styles/mapbox/streets-v11",
+            center: [37.61513, 55.7513461], // moscow
+            zoom: 11,
+        });
     });
 
     it('render correctly', function () {
